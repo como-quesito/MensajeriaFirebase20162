15e75b7d41b64ebf1497a44f532718fa16bf5cd5
campitos <rapidclimate@gmail.com>
2016-09-14 22:48:46 -0500

    tola

--- app/src/main/java/firebase2/campitos/org/mensajeriafirebase20162/MyFirebaseMessagingService.java
+++ app/src/main/java/firebase2/campitos/org/mensajeriafirebase20162/MyFirebaseMessagingService.java
@@ -53,6 +53,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification("Hola");
     }
     // [END receive_message]
 
